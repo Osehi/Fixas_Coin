9633f1635ee7ae45bee70bfaf822d32c1a914c46
Osehi <osehiaseehilen@gmail.com>
2020-02-27 20:45:05 +0100

    'networkService-created'

--- app/src/main/java/com/polish/fixascryptocoin/network/Service.kt
+++ app/src/main/java/com/polish/fixascryptocoin/network/Service.kt
@@ -1,2 +1,36 @@
 package com.polish.fixascryptocoin.network
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.polish.fixascryptocoin.model.CryptoCoin
+import kotlinx.coroutines.Deferred
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CryptoCoinService {
+
+    @GET("ticker")
+    fun getCryptoCoinRes(@Query("linit") limit:Int): Deferred<List<CryptoCoin>>
+
+}
+
+
+/*
+    The entry point for network access
+ */
+
+object CryptoCoinNetwork {
+
+    private const val URL = "https://api.coinmarketcap.com/v1/"
+
+    // Configure retrofit to parse JSON and use coroutines
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .build()
+
+    val cryptoCoins = retrofit.create(CryptoCoinService::class.java)
+
+}
