41c4a7fdbcff9fbd61c306a48bcfb1be374e5d6c
Osehi <osehiaseehilen@gmail.com>
2020-02-28 22:39:20 +0100

    'viewModel-test'

--- app/src/test/java/com/polish/fixascryptocoin/database/CryptoCoinDatabaseTest.kt
+++ app/src/test/java/com/polish/fixascryptocoin/database/CryptoCoinDatabaseTest.kt
@@ -1,5 +1,19 @@
 package com.polish.fixascryptocoin.database
 
+import android.os.Build
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
-class CryptoCoinDatabaseTest
+@ExperimentalCoroutinesApi
+@ObsoleteCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
+class CryptoCoinDatabaseTest {
+
+
+
+}
